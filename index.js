59f0c8249866e2af759f7a8f4870488ce695172a
David21x10 <davidfer21.yt@gmail.com>
2025-03-04 18:33:09 -0600

    Ejecutando el puerto

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const db= require('./app/config/db')
+const App = require ('./app/app');
+require('dotenv').config()
+
+const PORT = process.env.PORT || process.env.APP_PORT;
+
+//db.sequelizeInstance.sync()
+//.then(()=> {
+ //   console.info("Base de Datos sincronizada");
+    App.listen(parseInt(PORT), function (error){
+    if(error) return console.error(error);
+    console.info(`EL SERVIDOR SE ESTÁ EJECUTANDO EN EL PUERTO: ${PORT}`);
+});
+//})
+//.catch(error => console.error(error));
